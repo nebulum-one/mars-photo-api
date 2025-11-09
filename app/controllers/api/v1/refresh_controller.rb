81f67bd6d8bbb5b1cdab2446b2b2c8b74f2d1c3d
Nebulum <hq@nebulum.one>
2025-11-09 12:27:24 -0500

    Update refresh_controller.rb

--- app/controllers/api/v1/refresh_controller.rb
+++ app/controllers/api/v1/refresh_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::RefreshController < ApplicationController
   def create
-    PerseveranceScraper.new.fetch_latest
+    PerseveranceScraper.new.scrape
     render json: { status: "ok", updated_at: Time.now }
   end
 end
